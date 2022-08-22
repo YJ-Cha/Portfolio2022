8fea2a21ca65709ad9c30ad35c052109430632bf
YJ-Cha <toto941002@naver.com>
2022-08-22 20:53:24 +0900

    0822

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 new fullpage('#fullpage', {
 	//options here
 	autoScrolling: true,
+	scrollBar: false,
 	scrollHorizontally: true,
 	anchors: ['sec1', 'sec2', 'sec3', 'sec4', 'sec5'],
 	navigation: true,
@@ -11,7 +12,7 @@ new fullpage('#fullpage', {
 
 	//섹션 배경색, 이미지 로드
 	beforeLeave: function(origin, destination, direction, trigger){
-			$('.sectionBg').removeClass('act0 act1 act2 act3 act4');      
+			$('.sectionBg').removeClass('act0 act1 act2 act3 act4 act5 act6 act7 act8');      
 			$('.contentImg').slideUp(100);
 			$('.contentWrap').removeClass('on');      
 			$('#s0').removeClass('on');      
@@ -40,6 +41,22 @@ new fullpage('#fullpage', {
 			$('.sectionBg').addClass('act4');
 			$('.contentImg').slideDown();
 			$('.contentImg img').attr({src:'images/con4Img.png'});
+		} else if ($('#s5').hasClass('active')) {
+			$('.sectionBg').addClass('act5');
+			$('.contentImg').slideDown();
+			$('.contentImg img').attr({src:'images/con5Img.png'});
+		} else if ($('#s6').hasClass('active')) {
+			$('.sectionBg').addClass('act6');
+			$('.contentImg').slideDown();
+			$('.contentImg img').attr({src:'images/con6Img.png'});
+		} else if ($('#s7').hasClass('active')) {
+			$('.sectionBg').addClass('act7');
+			$('.contentImg').slideDown();
+			$('.contentImg img').attr({src:'images/con7Img.png'});
+		} else if ($('#s8').hasClass('active')) {
+			$('.sectionBg').addClass('act8');
+			$('.contentImg').slideDown();
+			$('.contentImg img').attr({src:'images/con8Img.png'});
 		};  
 		
 		$('.contentWrap').addClass('on');
@@ -81,3 +98,10 @@ $('.popupBg').click(function(){
 //     // scrollbars: false,
 // 	fadeScrollbars: true,
 // });
+
+
+//s4 새 창 사이즈 조절
+
+function openS4() {
+	window.open("https://yj-cha.github.io/asan/","아산병원 모바일","width=500, height=800, top=100, left=100");
+}
